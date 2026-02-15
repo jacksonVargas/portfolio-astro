81cd4483f379a903fe231692c6a59a07d36ba616
jackson vargas <jacksonvargas134@gmail.com>
2026-02-15 13:04:32 -0300

    teste

--- src/projects.ts
+++ src/projects.ts
@@ -1,6 +1,6 @@
 import type { ImageMetadata } from 'astro'
-import codigos from './assets/codigos.webp'
 import login from './assets/login.png'
+import devControle from './assets/dev-controle.png'
 
 export interface Project {
   id: number
@@ -19,5 +19,13 @@ export const projects: Project[] = [
     description: 'Sistema de login com email e senha mais login social com github usando o better-auth.',
     link: 'https://login-better-auth.vercel.app/login',
     git: 'https://github.com/jacksonVargas/login-better-auth'
-  }
+  },
+  {
+    id: 2,
+    cover: devControle,
+    title: 'DevControle',
+    description: 'Sistema para ter controle dos clientes e abrir chamados, feito com next usando api routes.',
+    link: 'https://dev-controle-app.vercel.app/',
+    git: 'https://github.com/jacksonVargas/dev_controle_app'
+  },
 ]
